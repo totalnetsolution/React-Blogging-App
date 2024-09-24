21c10819436cf9d78c2cda9cd26e2d9a9e20673a
totalnetsolution <153507938+totalnetsolution@users.noreply.github.com>
2024-09-25 02:50:26 +0500

    blogging app completed

--- src/firebase.js
+++ src/firebase.js
@@ -1,1 +1,12 @@
+// src/firebase.js
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
+
+
+
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
